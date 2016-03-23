6bf4d7ec9f2a8a2fb06c6a943ce47469cfb67ad8
Noelle Corbin <noelle01520@yahoo.com>
2016-03-23 15:41:23 -0400

    reverting

--- angular/settings/base.py
+++ angular/settings/base.py
@@ -15,21 +15,34 @@ import sys
 import angular as project_module
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.realpath(project_module.__file__))
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+PYTHON_BIN = os.path.dirname(sys.executable)
+ve_path = os.path.dirname(os.path.dirname(os.path.dirname(PROJECT_DIR)))
+# Assume that the presence of 'activate_this.py' in the python bin/
+# directory means that we're running in a virtual environment.
+if os.path.exists(os.path.join(PYTHON_BIN, 'activate_this.py')):
+    # We're running with a virtualenv python executable.
+    VAR_ROOT = os.path.join(os.path.dirname(PYTHON_BIN), 'var')
+elif ve_path and os.path.exists(os.path.join(ve_path, 'bin',
+        'activate_this.py')):
+    # We're running in [virtualenv_root]/src/[project_name].
+    VAR_ROOT = os.path.join(ve_path, 'var')
+else:
+    # Set the variable root to a path in the project which is
+    # ignored by the repository.
+    VAR_ROOT = os.path.join(PROJECT_DIR, 'var')
+
+if not os.path.exists(VAR_ROOT):
+    os.mkdir(VAR_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jadew0lfI3'
+SECRET_KEY = '#!r7w37b^y_@_om4rg-19ka84o^=9f2(@nuv31c&5mj%9hluko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,6 +144,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
+
+STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
+MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(ve_path, 'django1.9', 'basic-angular-django')
+)
 print(STATICFILES_DIRS)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',

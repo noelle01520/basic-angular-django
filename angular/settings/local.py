e92893f75a85f8e7cd3d3dcd299566068e0019bc
Noelle Corbin <noelle01520@yahoo.com>
2016-03-23 13:54:55 -0400

    updated settings

--- angular/settings/local.py
+++ angular/settings/local.py
@@ -10,12 +10,12 @@ STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'angular',                      # Or path to database file if using sqlite3.
+            'NAME': 'd96s1asano5p2s',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'angular',
-            'PASSWORD': 'angular',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
+            'USER': 'bfhhkfjddexayh',
+            'PASSWORD': 'MCplNsmir_132-l075SpMRbE3f',
+            'HOST': 'ec2-50-16-238-141.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                      # Set to empty string for default.
         },
 }
 # WSGI_APPLICATION = 'plan.wsgi.dev.application'

ac2ce8349cede38ec300a0f0c4b9bce9f79b661f
Noelle Corbin <noelle01520@yahoo.com>
2016-03-24 14:31:46 -0400

    added logging

--- angular/settings/local.py
+++ angular/settings/local.py
@@ -15,3 +15,19 @@ DATABASES = {
         },
 }
 # WSGI_APPLICATION = 'plan.wsgi.dev.application'
+# heroku logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}

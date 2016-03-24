a5f532581620357d6bff7578023a091ac9cb7ed8
Noelle Corbin <noelle01520@yahoo.com>
2016-03-24 06:38:29 -0400

    seems to be different on heroku

--- angular/settings/local.py
+++ angular/settings/local.py
@@ -3,22 +3,6 @@ from angular.settings.base import *
 
 DEBUG = True
 
-VAR_ROOT = 'var/www/angular'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-DIR_ARRAY = os.path.split(BASE_DIR)
-PROJECT_ROOT = DIR_ARRAY[0]
-print(PROJECT_ROOT)
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-MEDIA_ROOT = os.path.join(VAR_ROOT, 'uploads')
-#STATIC_ROOT = os.path.join(VAR_ROOT, 'static')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.

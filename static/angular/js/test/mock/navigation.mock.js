54cd2cac23a5d71afc208ff24935e383bb79f168
Noelle Corbin <noelle01520@yahoo.com>
2016-03-24 08:22:23 -0400

    removed extra static directory

--- static/angular/js/test/mock/navigation.mock.js
+++
@@ -1,9 +0,0 @@
-angular.module('NavigationMock', [])
-  .provider('Navigation', function(){
-    this.$get = function() {
-      return {
-        display: function() {},
-        goHome: function() {},
-      };
-    };
-  });

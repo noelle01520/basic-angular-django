1b475e651ccbb9aced6b326b83030392412f7936
Noelle Corbin <noelle01520@yahoo.com>
2016-03-24 05:08:38 -0400

    added js

--- angular/settings/base.py
+++ angular/settings/base.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/uploads/'
 
 STATIC_ROOT = os.path.join(VAR_ROOT, 'staticfiles')
